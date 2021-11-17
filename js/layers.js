d215e0e7ba28a8e8decd4d7e073d0a1432a78782
ThisIsNobode <94207037+ThisIsNobode@users.noreply.github.com>
2021-11-18 10:14:56 +1100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,14 +1,14 @@
-addLayer("p", {
+`addLayer("p", {`
     name: "Yes", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "Y", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0), 
     color: "yellow",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "yes coins", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    baseResource: "Coins", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
@@ -21,8 +21,15 @@ addLayer("p", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "y", description: "y: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true}
-})
+    layerShown(){return true},
+
+})```js
+    upgrades:11 {
+  title:More Coins "Make this whatever you want!",
+    description: "Double your Coin gain.",
+    cost: new Decimal(1),
+    },
+```
 
